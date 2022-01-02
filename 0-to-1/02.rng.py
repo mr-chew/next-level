f2b08b8680e425e13b684d5d6b533fbb71759d4a
chew <mr.krabs.6868@protonmail.com>
2022-01-02 14:42:04 +0800

    .

--- 0-to-1/02.rng.py
+++ 0-to-1/02.rng.py
@@ -8,9 +8,22 @@ a =1
 b =10
 s=1
 rnd.seed(s)
+print(f"using rnd.seed({s})")
 print(f"random int from {a} to {b} -> {rnd.randint(a, b)}")
 print(f"random int from {a} to {b} -> {rnd.randint(a, b)}")
 print(f"random int from {a} to {b} -> {rnd.randint(a, b)}")
 print(f"random int from {a} to {b} -> {rnd.randint(a, b)}")
 rnd.seed(s)
+print(f"random int from {a} to {b} -> {rnd.randint(a, b)}\n")
+
+state=rnd.getstate()
+rnd.setstate(state)
+print(f"using rnd.setstate()")
 print(f"random int from {a} to {b} -> {rnd.randint(a, b)}")
+print(f"random int from {a} to {b} -> {rnd.randint(a, b)}")
+print(f"random int from {a} to {b} -> {rnd.randint(a, b)}")
+rnd.setstate(state)
+print(f"random int from {a} to {b} -> {rnd.randint(a, b)}")
+print(f"random int from {a} to {b} -> {rnd.randint(a, b)}")
+print(f"random int from {a} to {b} -> {rnd.randint(a, b)}")
+
