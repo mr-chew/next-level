c658cc28b5533d746e3c38e1226be53cbcb1d14d
chew <mr.krabs.6868@protonmail.com>
2022-01-02 14:09:31 +0800

    Initial commit

---
+++ 0-to-1/02.rng.py
@@ -0,0 +1,15 @@
+# create a program that generates a random number using the random library
+# importing library
+import random as rnd
+
+print(f"random floating point {rnd.random()}")
+
+a =1
+b =10
+s=1
+rnd.seed(s)
+print(f"random int from {a} to {b} -> {rnd.randint(a, b)}")
+print(f"random int from {a} to {b} -> {rnd.randint(a, b)}")
+print(f"random int from {a} to {b} -> {rnd.randint(a, b)}")
+print(f"random int from {a} to {b} -> {rnd.randint(a, b)}")
+print(f"random int from {a} to {b} -> {rnd.randint(a, b)}")
