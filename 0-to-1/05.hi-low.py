c4b3f68c74dd3528a024f5bb08b74d7edb53874e
chew <mr.krabs.6868@protonmail.com>
2022-01-06 17:15:33 +0800

    .

--- 0-to-1/05.hi-low.py
+++ 0-to-1/05.hi-low.py
@@ -5,3 +5,31 @@ def computer_choice():
     return num
 
 # to reuse some of rock paper scissor while loop
+def get_input(prompt):
+    while True:
+        try:
+            value = int(input(prompt))
+        except:
+            print("Invalid input. Enter 1 to 100 only")
+            continue
+        
+        if value <1 or value >100:
+            print("Invalid input. Enter 1 to 100 only")
+            continue
+        else:
+            break
+    return value
+
+cc = computer_choice()
+
+
+attempt = True
+while attempt:
+    guess = get_input("Guess the number, Enter 1 to 100 -> ")
+    if guess < cc:
+        print("Your guess is too low")
+    elif guess > cc:
+        print("Your guess is too high")
+    else:
+        print(f"You got it, the number is {cc}")    
+        attempt = False
