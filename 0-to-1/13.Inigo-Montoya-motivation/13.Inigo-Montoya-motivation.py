cc26da612af134c6735f22ef0cf8749819540d54
mr-chew <mr.krabs.6868@protonmail.com>
2022-01-13 14:18:26 +0800

    .

--- 0-to-1/13.Inigo-Montoya-motivation/13.Inigo-Montoya-motivation.py
+++ 0-to-1/13.Inigo-Montoya-motivation/13.Inigo-Montoya-motivation.py
@@ -4,4 +4,4 @@ from pathlib import Path
 data_folder = Path("data/")
 file_to_open = data_folder / "motivation.txt"
 quotes = file_to_open.read_text().splitlines()
-print(random.choice(quotes))
+print(random.choice(quotes)
