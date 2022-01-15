b1597fadd0c36cf1a91e206389a09cb45b8c0f65
mr-chew <mr.krabs.6868@protonmail.com>
2022-01-15 23:04:31 +0800

    appended data now added in correct json format.

--- 0-to-1/03.contact/03.contact-json.py
+++ 0-to-1/03.contact/03.contact-json.py
@@ -6,7 +6,7 @@ file_to_open = data_folder / "contact.json"
 
 
 def add_contact(contact, filename):
-    with open(filename, "a") as f:
+    with open(filename, "w") as f:
         json.dump(contact, f, indent=2)
         
 print(f"Adding contacts...")        
@@ -14,7 +14,7 @@ name = input("Name please ")
 phone = input("mobile no. ")
 email = input("Email ")
 contact = {
-    "employees" : [
+    "user": [
         {
             "Name": name,
             "Phone": phone,
@@ -23,4 +23,15 @@ contact = {
     ]
 }
 
-add_contact(contact, "03.contact.json")
+new_contact = []
+if Path(file_to_open).is_file():
+    # print("file exist")
+    with open(file_to_open) as f:
+        contact_record = json.load(f)
+    
+    contact_record.append(contact)
+    add_contact(contact_record, file_to_open)
+else:
+    # print("file not found")
+    new_contact.append(contact)
+    add_contact(new_contact, file_to_open)
